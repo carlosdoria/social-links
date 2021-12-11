5acd0cef53aed12cf1f3cc5045cf869e4c8abc08
carlosdoria <carlosdoria953@gmail.com>
2021-12-11 02:10:25 -0300

    feat: test

--- src/pages/api/links.ts
+++ src/pages/api/links.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import { title } from 'process'
 
 interface link {
   title: string
@@ -9,41 +8,30 @@ interface link {
 }
 
 type Data = {
-  links: Array<link> | any
+  links: Array<link>
 }
 
 export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  try {
-    const doc = new GoogleSpreadsheet( '1EdMDQrvUtWqTtan9aUUVcFymYxNSiHe1iPAmbR9hrIc' )
+  const doc = new GoogleSpreadsheet( '1EdMDQrvUtWqTtan9aUUVcFymYxNSiHe1iPAmbR9hrIc' )
 
-    await doc.useServiceAccountAuth( {
-      client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL as string,
-      private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY as string,
-    } )
+  await doc.useServiceAccountAuth( {
+    client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL as string,
+    private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY as string,
+  } )
 
-    await doc.loadInfo() // loads document properties and worksheets
-    const sheet = doc.sheetsByIndex[ 0 ]
-    const rows = await sheet.getRows()
+  await doc.loadInfo() // loads document properties and worksheets
+  const sheet = doc.sheetsByIndex[ 0 ]
+  const rows = await sheet.getRows()
 
-    const links = rows.map( item => ( {
-      title: item.Title,
-      link: item.Links
-    } ) )
+  const links = rows.map( item => ( {
+    title: item.Title,
+    link: item.Links
+  } ) )
 
-    res.status( 200 ).json( {
-      links: [
-        {
-          title: 'teste',
-          link: 'test'
-        }
-      ]
-    } )
-  } catch ( err ) {
-    res.status( 400 ).json( {
-      links: err
-    } )
-  }
+  res.status( 200 ).json( {
+    links
+  } )
 }
