568400a90e3a6a4d110172db84e55c3c06da5f9d
carlosdoria <carlosdoria953@gmail.com>
2021-12-11 02:05:37 -0300

    feat: test

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -5,5 +5,8 @@ export const ApiGithub = axios.create( {
 } )
 
 export const ApiGoogleSheets = axios.create( {
-  baseURL: 'https://social-links-o51t3hzsw-carlosdoria.vercel.app/api'
+  baseURL: 'https://social-links-o51t3hzsw-carlosdoria.vercel.app/api',
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
 } )
